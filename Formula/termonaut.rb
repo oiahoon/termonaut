efcd1db24a6be4a13022650b6ebbe7bd6f1e8e85
Joey Huang <johuang@aligntech.com>
2025-06-18 16:28:55 +0800

    Complete Homebrew formula with real checksums and working installation

--- Formula/termonaut.rb
+++ Formula/termonaut.rb
@@ -1,20 +1,20 @@
 class Termonaut < Formula
   desc "Gamified terminal productivity tracker with achievements and XP system"
   homepage "https://github.com/oiahoon/termonaut"
-  url "https://github.com/oiahoon/termonaut/releases/download/v0.9.0/termonaut-v0.9.0-darwin-amd64.tar.gz"
-  sha256 "PLACEHOLDER_SHA256_AMD64"
+  url "https://github.com/oiahoon/termonaut/releases/download/v0.9.0/termonaut-0.9.0-darwin-amd64.tar.gz"
+  sha256 "8ad635b7e67ba59abbb1567b47de77421dda26cba9490ae5bf0cdb5f6f3401f0"
   license "MIT"
   version "0.9.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/oiahoon/termonaut/releases/download/v0.9.0/termonaut-v0.9.0-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_AMD64"
+      url "https://github.com/oiahoon/termonaut/releases/download/v0.9.0/termonaut-0.9.0-darwin-amd64.tar.gz"
+      sha256 "8ad635b7e67ba59abbb1567b47de77421dda26cba9490ae5bf0cdb5f6f3401f0"
     end
 
     on_arm do
-      url "https://github.com/oiahoon/termonaut/releases/download/v0.9.0/termonaut-v0.9.0-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_ARM64"
+      url "https://github.com/oiahoon/termonaut/releases/download/v0.9.0/termonaut-0.9.0-darwin-arm64.tar.gz"
+      sha256 "6a9507c8b74efb1052feb9e19f717b1414dbd8e716eb9f81c1c785d80bc1a5a9"
     end
   end
 
@@ -31,10 +31,10 @@ class Termonaut < Formula
   end
 
   def install
-    bin.install "termonaut-darwin-amd64" => "termonaut" if OS.mac? && Hardware::CPU.intel?
-    bin.install "termonaut-darwin-arm64" => "termonaut" if OS.mac? && Hardware::CPU.arm?
-    bin.install "termonaut-linux-amd64" => "termonaut" if OS.linux? && Hardware::CPU.intel?
-    bin.install "termonaut-linux-arm64" => "termonaut" if OS.linux? && Hardware::CPU.arm?
+    bin.install "termonaut-0.9.0-darwin-amd64" => "termonaut" if OS.mac? && Hardware::CPU.intel?
+    bin.install "termonaut-0.9.0-darwin-arm64" => "termonaut" if OS.mac? && Hardware::CPU.arm?
+    bin.install "termonaut-0.9.0-linux-amd64" => "termonaut" if OS.linux? && Hardware::CPU.intel?
+    bin.install "termonaut-0.9.0-linux-arm64" => "termonaut" if OS.linux? && Hardware::CPU.arm?
 
     # Create symlink for short command
     bin.install_symlink "termonaut" => "tn"
@@ -66,13 +66,13 @@ class Termonaut < Formula
 
   test do
     # Test basic functionality
-    assert_match "termonaut", shell_output("#{bin}/termonaut --version")
-    assert_match "termonaut", shell_output("#{bin}/tn --version")
+    assert_match "Termonaut", shell_output("#{bin}/termonaut version")
+    assert_match "Termonaut", shell_output("#{bin}/tn version")
 
     # Test help command
     assert_match "Usage:", shell_output("#{bin}/termonaut --help")
 
     # Test that the binary is executable
-    system "#{bin}/termonaut", "--version"
+    system "#{bin}/termonaut", "version"
   end
 end
